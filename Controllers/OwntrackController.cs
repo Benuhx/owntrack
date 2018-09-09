bd1f86ec44f17a39c7056a2b673df6569dd6dba9
Jannis Mühlemeyer <jannis.muehlemeyer@outlook.com>
2018-09-09 15:38:24 +0200

    Logging for OwnTrack Controller

--- Controllers/OwntrackController.cs
+++ Controllers/OwntrackController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace owntrack.Controllers
 {
@@ -10,6 +11,12 @@ namespace owntrack.Controllers
     [ApiController]
     public class OwntrackController : ControllerBase
     {
+        private readonly ILogger<OwntrackController> logger;
+
+        public OwntrackController(ILogger<OwntrackController> logger) {
+            this.logger = logger;
+        }
+
         // Just for testing
         [HttpGet]
         public string Get() {
@@ -20,7 +27,11 @@ namespace owntrack.Controllers
         // Owntrack Docs: https://owntracks.org/booklet/tech/json/
         [HttpPost]
         public void Post([FromBody] OwntrackData owntrackData) {
+            logger.LogInformation("Owntrack HTTP POST request received");
+
             
+
+            logger.LogInformation("Request done");
         }        
     }
 }
