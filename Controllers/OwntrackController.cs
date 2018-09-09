08d06ffbfb878370ffd8408f5312818e11788283
Jannis Mühlemeyer <jannis.muehlemeyer@outlook.com>
2018-09-09 15:00:02 +0200

    Rename Controller

---
+++ Controllers/OwntrackController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace owntrack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwntrackController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() {
+            return "Test erfolgreich";
+        }
+        
+        [HttpPost]
+        public void Post([FromBody] string value) {
+
+        }        
+    }
+}
