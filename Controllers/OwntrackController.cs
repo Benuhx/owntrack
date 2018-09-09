5c5714ddd273a00d1796ea6eb34274a79e917015
Jannis Mühlemeyer <jannis.muehlemeyer@outlook.com>
2018-09-09 19:00:46 +0200

    Implemented MqttClient

--- Controllers/OwntrackController.cs
+++ Controllers/OwntrackController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using owntrack.MQTT;
 
 namespace owntrack.Controllers
 {
@@ -12,26 +14,36 @@ namespace owntrack.Controllers
     public class OwntrackController : ControllerBase
     {
         private readonly ILogger<OwntrackController> logger;
-
-        public OwntrackController(ILogger<OwntrackController> logger) {
+        private readonly MqttClient mqttClient;
+        public OwntrackController(ILogger<OwntrackController> logger, ILogger<MqttClient> loggerMqttClient, IOptions<MqttSettings> mqttSettings)
+        {            
             this.logger = logger;
+            this.mqttClient = new MqttClient(loggerMqttClient, mqttSettings.Value);
         }
 
         // Just for testing
         [HttpGet]
-        public string Get() {
+        public string Get()
+        {
             return "Server is active";
         }
-        
+
         // Owntracks sends HTTP Post Request with the schema defined in OwntrackData.cs
         // Owntrack Docs: https://owntracks.org/booklet/tech/json/
         [HttpPost]
-        public void Post([FromBody] OwntrackData owntrackData) {
+        public void Post([FromBody] OwntrackData owntrackData)
+        {
             logger.LogInformation("Owntrack HTTP POST request received");
 
-            
+            var mqttMsg = new MqttMessage
+            {
+                Topic = "mobile/fire/batteryLevel",
+                Message = Convert.ToString(owntrackData.Batt)
+            };
+
+            mqttClient.QueueMessage(mqttMsg);
 
             logger.LogInformation("Request done");
-        }        
+        }
     }
 }
